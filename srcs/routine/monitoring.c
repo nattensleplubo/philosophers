869b758f966f811cd607c0866bd8ed3a9984612f
Nathan GOBERT <ngobert@e2r1p14.clusters.42paris.fr>
2022-06-07 18:15:17 +0200

    ajoute 1 truc #dinguerie #quedusale #delire #lol #xd #mdr

--- srcs/routine/monitoring.c
+++ srcs/routine/monitoring.c
@@ -6,9 +6,35 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:09:35 by ngobert           #+#    #+#             */
-/*   Updated: 2022/06/07 14:22:25 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/06/07 17:03:45 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	check_if_one_is_dead(t_general *general)
+{
+	 int	i;
+
+	 i = 0;
+	 while (i < general->number_of_philo)
+	 {
+		if (check_last_meal(&general->philos[i]) == ERROR)
+		i++;
+	 }
+}
+
+void	*begin_monitoring(void *arg)
+{
+	t_general	*data;
+
+	data = (t_general *)arg;
+	while (1)
+	{
+		pthread_mutex_lock(&data->mutex);
+		if (check_if_one_is_dead(data))
+		{
+			//quitter
+		}
+	}
+}
