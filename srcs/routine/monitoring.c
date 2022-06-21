182eee8ddac8300a67ecc7fbe929cf5f25b396a2
Nathan GOBERT <ngobert@e2r7p8.42.fr>
2022-06-21 14:48:52 +0200

    m355age 2 m0r

--- srcs/routine/monitoring.c
+++ srcs/routine/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:09:35 by ngobert           #+#    #+#             */
-/*   Updated: 2022/06/17 14:57:44 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/06/21 14:00:57 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	check_last_meal(t_philo *philo)
 {
 	if (get_time() - philo->general->starting_time > philo->time_to_die)
 	{
-		dprintf(2, BRED"👹 DEATH FROM %d 👹\n"CRESET, philo->id);
+		dprintf(1, BRED"%d %d died\n"CRESET, get_time() - philo->general->starting_time, philo->id);
 		philo->general->is_dead = 1;
 		return (0);
 	}
